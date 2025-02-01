d32b910132bc2dd33e5a58e99be477d615466b23
Tung Nguyen <2715595+lukaz17@users.noreply.github.com>
2025-02-01 15:55:56 +0700

    feat(random): string pseudo-random

---
+++ random/pseudorng/string_rand.go
@@ -0,0 +1,74 @@
+// Copyright (C) 2025 T-Force I/O
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pseudorng
+
+import "strings"
+
+// Base64 returns a pseudo-random base64 string with charset defined in RFC 4648 for UTF-8
+// from the default [Source].
+// The length (n) denotes number of bytes the base64 string represented, not the
+// actual number of characters of the generated base64 string.
+// Available since v0.3.0
+func Base64(n uint) string {
+	charset := []rune{
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+		'+', '/',
+	}
+	ret := String(n+ceilUint(n, 3), charset)
+	switch n % 3 {
+	case 1:
+		return ret + "=="
+	case 2:
+		return ret + "="
+	default:
+		return ret
+	}
+}
+
+// Hex returns a pseudo-random hex string in lower case
+// from the default [Source].
+// The length (n) denotes number of bytes the hex string represented, not the
+// actual number of characters of the generated hex string.
+// Available since v0.3.0
+func Hex(n uint) string {
+	charset := []rune{
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+		'a', 'b', 'c', 'd', 'e', 'f',
+	}
+	return String(n*2, charset)
+}
+
+// String returns a pseudo-random string with specific length (n) and based on
+// pre-defined list of characters from the default [Source].
+// The length of charset should be less than 2³¹ for reliability.
+// The probability for appearances of each character cannot be customized.
+// Available since v0.3.0
+func String(n uint, charset []rune) string {
+	var sb strings.Builder
+	charcount := len(charset)
+	for i := uint(0); i < n; i++ {
+		sb.WriteRune(charset[Intn(charcount)])
+	}
+	return sb.String()
+}
+
+// ceilUint returns ceiling result of x / y as same math.Ceil.
+// See https://stackoverflow.com/a/2745086 for more information.
+// Available since v0.3.0
+func ceilUint(x, y uint) uint {
+	return 1 + ((x - 1) / y)
+}
