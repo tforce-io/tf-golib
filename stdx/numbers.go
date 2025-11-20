9f0fa41f76e95b7a6fea9d92f4fb60cdffa2a4fd
Tung Nguyen <2715595+lukaz17@users.noreply.github.com>
2025-11-20 16:24:10 +0700

    feat(stdx): simplify uint256 definition

--- stdx/numbers.go
+++ stdx/numbers.go
@@ -12,24 +12,16 @@ import (
 
 // A Uint256 is wrapped *big.Int to intergrate with system that use large number and provide fluent apis for type casting.
 // Available since v0.1.0
-type Uint256 struct {
-	value *big.Int
-}
+type Uint256 big.Int
 
 // Cast Uint256 to *big.Int.
 // Available since v0.1.0
 func (s *Uint256) BigInt() *big.Int {
-	return s.value
-}
-
-// Cast Uint256 to uint64. This may cause data loss when the value is larger than 18446744073709551615.
-// Available since v0.1.0
-func (s *Uint256) Uint64() uint64 {
-	return s.value.Uint64()
+	return (*big.Int)(s)
 }
 
 // Return underlying value of Bytes as *big.Int.
 // Available since v0.1.0
 func (s *Uint256) Value() *big.Int {
-	return s.value
+	return (*big.Int)(s)
 }
